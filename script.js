6d472ca6edfa3ce5731a62de0d8718b6673fa7ca
AlphaCfter <aku.ajith.kumar.ms@gmail.com>
2024-12-06 21:04:58 +0530

    quick deployable project

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("nav ul li a").on("click", function() {
+
+        $("nav ul li a").removeClass("active");
+        
+
+        $(this).addClass("active");
+    });
+});
